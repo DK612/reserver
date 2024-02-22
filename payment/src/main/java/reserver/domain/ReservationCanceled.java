7c253f77f931a010ce60597afd53a37508fcb2f4
Dongwook-Kim <44344392+DK612@users.noreply.github.com>
2024-02-22 11:10:18 +0900

    commit

--- payment/src/main/java/reserver/domain/ReservationCanceled.java
+++ payment/src/main/java/reserver/domain/ReservationCanceled.java
@@ -11,5 +11,10 @@ public class ReservationCanceled extends AbstractEvent {
 
     private Long id;
     private Long userId;
+    private Long lodgingId;
+    private Long pricePerDay;
+    private Date startDt;
+    private Date endDt;
+    private Boolean isChecked;
     private Boolean isCanceled;
 }
